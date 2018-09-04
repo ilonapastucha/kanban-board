336734d4bb74a606638c9b53b681d58b932cf265
Ilona Pastucha <ilonka.p4@gmail.com>
2018-09-04 20:29:23 +0200

    Fix js bugs

--- js/App.js
+++ js/App.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
-    var myHeaders = {
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
     'X-Client-Id': '3507',
     'X-Auth-Token': '0e7f2728083188aaf56325802540cc4b',
     //'Content-Type': 'application/json; charset=utf-8'
-    };
+};
 
+ //generuje szablon
+function generateTemplate(name, data, basicElement) {
+    var template = document.getElementById(name).innerHTML;
+    var element = document.createElement(basicElement || 'div');
+  
+    Mustache.parse(template);
+    element.innerHTML = Mustache.render(template, data);
+  
+    return element;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
     // funkcja odpytująca serwer o zasób tablicy
     fetch(baseUrl + '/board', { headers: myHeaders })
         .then(function(resp) {
@@ -25,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
             setupCards(col, column.cards); //dodaje kartę do kolumny
         });
     }
-  // tworzy karty w kolumnie
+    // tworzy karty w kolumnie
     function setupCards(col, cards) {
         cards.forEach(function (card) {
         var cardObj = new Card(card.id, card.name);
@@ -33,14 +43,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
-    //generuje szablon
-    function generateTemplate(name, data, basicElement) {
-      var template = document.getElementById(name).innerHTML;
-      var element = document.createElement(basicElement || 'div');
-    
-      Mustache.parse(template);
-      element.innerHTML = Mustache.render(template, data);
-    
-      return element;
-    }
 })
--- js/Card.js
+++ js/Card.js
@@ -1,81 +1,81 @@
 // tworzenie klasy card
 function Card(id, name) {
-    var self = this; 
-  
+    var self = this;
+
     this.id = id;
     this.name = name || 'No name given';
     this.element = generateTemplate('card-template', {
-      description: this.name,
-      id: this.id
+        description: this.name,
+        id: this.id
     }, 'li') // nazwa: card-template, li jako element ul którą jest nasza kolumna
-  
+
     this.element.querySelector('.card').addEventListener('click', function (event) {
-      event.stopPropagation();
-  
-      if (event.target.classList.contains('btn-delete')) {
-        self.removeCard();
-  
-      }
+        event.stopPropagation();
+
+        if (event.target.classList.contains('btn-delete')) {
+            self.removeCard();
+
+        }
     });
-  
+
     this.element.querySelector('.card').addEventListener('dblclick', function (event) {
-      event.stopPropagation();
-      self.update();
+        event.stopPropagation();
+        self.update();
     });
-  
-  }
-   // usunięcie karty z serwera
-  Card.prototype = {
+
+}
+// usunięcie karty z serwera
+Card.prototype = {
     removeCard: function () {
-      var self = this;
-  
-      fetch(baseUrl + '/card/' + self.id, {
-          method: 'DELETE',
-          headers: myHeaders
-        })
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (resp) {
-          self.element.parentNode.removeChild(self.element);
-        })
+        var self = this;
+
+        fetch(baseUrl + '/card/' + self.id, {
+                method: 'DELETE',
+                headers: myHeaders
+            })
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (resp) {
+                self.element.parentNode.removeChild(self.element);
+            })
     },
-  
+
     update: function (targetList) {
-      var self = this;
-  
-      if (event.type == 'dblclick') {
-        var newName = prompt('Enter new name');
-      }
-  
-      var columnId = targetList;
-      var data = new FormData();
-  
-      data.append('name', newName || this.name);
-      data.append('bootcamp_kanban_column_id', columnId);
-      var contentType = {
-        'Content-Type': 'application/json; charset=utf-8'
-      }
-  
-      newHeaders = Object.assign({}, myHeaders);
-      newHeaders['Content-Type'] = 'application/json; charset=utf8';
-  
-  
-  
-      fetch(baseUrl + '/card/' + self.id, {
-          method: 'PUT',
-          headers: newHeaders,
-          body: data
-        })
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (resp) {
-  
-          self.element.querySelector('.card-description').innerTEXT = newName;
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
+        var self = this;
+
+        if (event.type == 'dblclick') {
+            var newName = prompt('Enter new name');
+        }
+
+        var columnId = targetList;
+        var data = new FormData();
+
+        data.append('name', newName || this.name);
+        data.append('bootcamp_kanban_column_id', columnId);
+        var contentType = {
+            'Content-Type': 'application/json; charset=utf-8'
+        }
+
+        newHeaders = Object.assign({}, myHeaders);
+        newHeaders['Content-Type'] = 'application/json; charset=utf8';
+
+
+
+        fetch(baseUrl + '/card/' + self.id, {
+                method: 'PUT',
+                headers: newHeaders,
+                body: data
+            })
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (resp) {
+
+                self.element.querySelector('.card-description').innerTEXT = newName;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
-  }
+}
